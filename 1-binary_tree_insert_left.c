dd284b6efe6d4f7b62e76269cf57dfc3936d234d
LeRoy-M <lmwasela@gmail.com>
2023-08-02 11:56:48 +0300

    1. Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,7 +22,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left)
 	{
 		l_leave->left = parent->left;
-		parent->left->parent = l_leave;
+		l_leave->left->parent = l_leave;
 	}
 
 	parent->left = l_leave;
