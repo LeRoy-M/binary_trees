1419af419f185a2ded227e31d3e5419c871a2b7a
LeRoy-M <lmwasela@gmail.com>
2023-08-08 11:02:28 +0300

    9. Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,9 +10,8 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l_height, r_height;
+	size_t l_height = 0, r_height = 0;
 
-	l_height = 0, r_height = 0;
 	if (!tree)
 		return (0);
 
